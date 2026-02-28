0b2d2b97473ee523ce811d05825583d94a8e80c7
madevkumar <madevkumarb35@gmail.com>
2026-02-28 16:08:11 +0530

    Added PlayerController.cs to Assets/Scripts/

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,6 @@
 using UnityEngine;
 
-/// <summary>
-/// PlayerController handles core player movement and platforming mechanics.
-/// Integrates with TimeLoopManager for time manipulation.
-/// </summary>
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
@@ -23,59 +18,48 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private bool jumpInput;
 
-    private void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
         timeLoopManager = FindObjectOfType<TimeLoopManager>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         HandleInput();
         CheckGrounded();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         Move();
         ApplyDrag();
     }
 
-    private void HandleInput()
-    {
+    private void HandleInput() {
         horizontalInput = Input.GetAxis("Horizontal");
         jumpInput = Input.GetKeyDown(KeyCode.Space);
-
-        if (jumpInput && isGrounded)
-        {
+        if (jumpInput && isGrounded) {
             Jump();
         }
     }
 
-    private void Move()
-    {
+    private void Move() {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
     }
 
-    private void Jump()
-    {
+    private void Jump() {
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    private void ApplyDrag()
-    {
+    private void ApplyDrag() {
         rb.drag = isGrounded ? groundDrag : airDrag;
     }
 
-    private void CheckGrounded()
-    {
+    private void CheckGrounded() {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundDragDistance, groundLayer);
         isGrounded = hit.collider != null;
     }
 
-    public void ResetPosition(Vector3 position)
-    {
+    public void ResetPosition(Vector3 position) {
         transform.position = position;
         rb.velocity = Vector3.zero;
     }
